d467d0829f50a30676d6dc7c8780badf9df6f8b0
Abdulaziz2004 <abu556626@gmail.com>
2022-09-03 23:56:14 +0500

    first comment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import ContinuousSlider from './Slider'
-
+import logo from "./logo.svg";
+import "./App.css";
+import ContinuousSlider from "./Slider";
 
 function App() {
-
   return (
     <div className="App">
-      
-     <h1>Lets code tamil</h1>
-     <ContinuousSlider />
+      <ContinuousSlider />
     </div>
   );
 }
--- src/Slider.js
+++ src/Slider.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Slider from '@material-ui/core/Slider';
-import FiberManualRecordOutlinedIcon from '@material-ui/icons/FiberManualRecordOutlined';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Slider from "@material-ui/core/Slider";
+import FiberManualRecordOutlinedIcon from "@material-ui/icons/FiberManualRecordOutlined";
 
 const useStyles = makeStyles({
-  root: {
-
-  },
+  root: {},
 });
 
 export default function ContinuousSlider() {
@@ -17,11 +15,11 @@ export default function ContinuousSlider() {
   const [number, setNumber] = React.useState(1);
 
   const handleChange = (event, newValue) => {
-      let newData = (newValue * (36/101)).toFixed(0)
-      if (newData == 0) {
-          newData = 1
-      }
-setNumber(newData)
+    let newData = (newValue * (36 / 101)).toFixed(0);
+    if (newData == 0) {
+      newData = 1;
+    }
+    setNumber(newData);
     setValue(newValue);
   };
 
@@ -29,21 +27,27 @@ setNumber(newData)
     <div className={classes.root}>
       <Typography id="continuous-slider" gutterBottom>
         360 Image view <br />
-  
       </Typography>
-      <img src={require(`./Car-Images/car-${number}.png`).default} width='800' height='400' />
+      <img
+        src={require(`./Car-Images/car-${number}.png`).default}
+        width="800"
+        height="400"
+      />
       <Grid container spacing={2}>
         <Grid item>
           <FiberManualRecordOutlinedIcon />
         </Grid>
         <Grid item xs>
-          <Slider value={value} onChange={handleChange} aria-labelledby="continuous-slider" />
+          <Slider
+            value={value}
+            onChange={handleChange}
+            aria-labelledby="continuous-slider"
+          />
         </Grid>
         <Grid item>
-        <FiberManualRecordOutlinedIcon />
+          <FiberManualRecordOutlinedIcon />
         </Grid>
       </Grid>
-      
     </div>
   );
 }
